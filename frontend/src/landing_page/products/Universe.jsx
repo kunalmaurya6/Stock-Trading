4963cba84e6d88f1a4f26628eb9c26a23986abae
kunalmaurya6 <mauryaankit86810@gmail.com>
2026-01-26 18:00:48 +0530

    prodect page is done

--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -11,28 +11,42 @@ const Universe = () => {
         </p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/zerodhaFundhouse.png" style={{width:"40%"}}/>
+          <p className="text-small text-muted">Our asset management venture
+            <br />that is creating simple and transparent index
+            <br />funds to help you save for your goals.</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/sensibullLogo.svg" />
+          <p className="text-small text-muted">Options trading platform that lets you
+            <br />create strategies, analyze positions, and examine
+            <br />data points like open interest, FII/DII, and more.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/goldenpiLogo.png" />
+          <p className="text-small text-muted">Investment research platform
+            <br />that offers detailed insights on stocks,
+            <br />sectors, supply chains, and more.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/streakLogo.png"  style={{width:"40%"}}/>
+          <p className="text-small text-muted">Systematic trading platform
+            <br />that allows you to create and backtest
+            <br />strategies without coding.</p>
         </div>
         <div className="col-4 p-3 mt-5">
           <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <p className="text-small text-muted">Thematic investing platform
+            <br />that helps you invest in diversified
+            <br />baskets of stocks on ETFs.</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/dittoLogo.png"  style={{width:"40%"}}/>
+          <p className="text-small text-muted">Personalized advice on life
+            <br />and health insurance. No spam
+            <br />and no mis-selling.</p>
         </div>
         <button
           className="p-2 btn btn-primary fs-5 mb-5"
