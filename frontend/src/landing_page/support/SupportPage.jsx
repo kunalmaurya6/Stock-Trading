ae9f11de5efe7b265507e956052664f3b316b67d
kunalmaurya6 <mauryaankit86810@gmail.com>
2026-01-27 15:48:50 +0530

    support page

--- frontend/src/landing_page/support/SupportPage.jsx
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from './Hero'
+import CreateTicket from './CreateTicket'
 
 const SupportPage = () => {
   return (
     <div>
-      support
+      <Hero/>
+      <CreateTicket/>
     </div>
   )
 }
