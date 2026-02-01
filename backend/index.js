f4206e98a97d32853a147d7831c4b1eb5a187ec5
kunalmaurya6 <mauryaankit86810@gmail.com>
2026-02-01 22:24:02 +0530

    post setup

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const cors = require("cors")
 
 const { HoldingsModel } = require('./model/HoldingsModel');
 const { PositionsModel } = require('./model/PositionsModel');
+const { OrdersModel } = require('./model/OrdersModel');
+
 
 const PORT = process.env.PORT || 3001;
 const uri = process.env.MONGO_URL;
@@ -197,6 +199,18 @@ app.get("/allHoldings", async (req, res) => {
 app.get("/allPositions", async (req, res) => {
     let allPositions = await PositionsModel.find({});
     res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+    let newOrder = new OrdersModel({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
+
+    newOrder.save();
+    res.send("Order saved");
 });
 
 app.listen(PORT, () => {
